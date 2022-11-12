17bcbc7086255d781e10f0bbd090ca016302035c
@alessandro-cav <alessandrocav14@gmail.com>
2022-11-12 13:39:43 -0300

    Criando o projeto crm-moremac

---
+++ src/main/java/br/com/crm/moremac/CrmMoremacApplication.java
@@ -0,0 +1,13 @@
+package br.com.crm.moremac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmMoremacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmMoremacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crm/moremac/CrmMoremacApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crm.moremac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmMoremacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
