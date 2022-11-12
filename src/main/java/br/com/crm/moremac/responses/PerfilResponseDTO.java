be0b7541a4ac2038c314c1b5dda4bd81bbf24e0a
@alessandro-cav <alessandrocav14@gmail.com>
2022-11-12 16:05:19 -0300

    Faltou implemntar essa classe do perfil

--- src/main/java/br/com/crm/moremac/responses/PerfilResponseDTO.java
+++ src/main/java/br/com/crm/moremac/responses/PerfilResponseDTO.java
@@ -1,5 +1,28 @@
 package br.com.crm.moremac.responses;
 
-public class PerfilResponseDTO {
+import java.io.Serializable;
 
+public class PerfilResponseDTO   implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
+
